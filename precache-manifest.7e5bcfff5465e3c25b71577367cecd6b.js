2252122568ac83a7003d1513aeb36ba5ef8b9225
rkh4 <rhys_howard@msn.com>
2020-05-09 10:26:37 +0100

    Updates

---
+++ precache-manifest.7e5bcfff5465e3c25b71577367cecd6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a16dd94e31f1ec824e78b070c778828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5257a740bedfa47c670",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "8734a35d095175533695",
+    "url": "/static/js/2.7c267f51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7c267f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5257a740bedfa47c670",
+    "url": "/static/js/main.ebe08d54.chunk.js"
+  },
+  {
+    "revision": "07328a03ae850c2e9c5e",
+    "url": "/static/js/runtime-main.0da35d89.js"
+  },
+  {
+    "revision": "b078b5b93d7c60fdf6265f6102adea5d",
+    "url": "/static/media/house1.b078b5b9.jpeg"
+  },
+  {
+    "revision": "7de22b224084795fa2f4e34b2ec5a3a9",
+    "url": "/static/media/logoWhite.7de22b22.svg"
+  }
+]);
